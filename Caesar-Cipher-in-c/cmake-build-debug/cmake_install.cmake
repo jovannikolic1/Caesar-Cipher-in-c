090352be3fd817aae6a2d231a3322ab58d0b2c96
JovanNikolic <jnikolic7723ri@raf.rs>
2024-09-07 21:40:08 +0200

    ok

--- Caesar-Cipher-in-c/cmake-build-debug/cmake_install.cmake
+++ Caesar-Cipher-in-c/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/jovannikolic/Desktop/Caesar-Cipher-in-c
+# Install script for directory: /Users/jovannikolic/Desktop/Caesar-Cipher-in-c/Caesar-Cipher-in-c
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/jovannikolic/Desktop/Caesar-Cipher-in-c/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/jovannikolic/Desktop/Caesar-Cipher-in-c/Caesar-Cipher-in-c/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
